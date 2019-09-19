d46bd7be06951fc9a10895166eae08ca314a5dab
Matt Plichta <bigcatplichta@gmail.com>
2019-09-19 14:59:23 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,10 +3,23 @@ class Dog
   
   def initialize(name)
     @name = name 
-    @@all << self 
+    self.save
   end
   
   def self.all 
     @@all
   end 
+  
+  def self.print_all
+    self.all.each {|d| puts d.name}
+  end 
+  
+  def self.save
+    @@all << self 
+  end 
+  
+  def self.clear_all
+      
+  end 
+  
 end
